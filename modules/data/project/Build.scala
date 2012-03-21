5f674993b7aeecae52919caafb2f37110ac134bd
Bayo Erinle <berinle@gmail.com>
2012-03-20 22:47:43 -0400

    cleaned up sub module build file

--- modules/data/project/Build.scala
+++ modules/data/project/Build.scala
@@ -4,28 +4,15 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "play-lombok"
+    val appName         = "data"
     val appVersion      = "1.0-SNAPSHOT"
-
-    val dataDependencies = Seq(
-    	"org.projectlombok" % "lombok" % "0.10.8"
-    	)
-
-    val dataProject = PlayProject(
-	    appName + "-data",
-	    appVersion,
-	    dataDependencies,
-	    path = file("modules/data"),
-	    mainLang = JAVA
-	  )
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      //"org.projectlombok" % "lombok" % "0.10.8"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
-    ).dependsOn(dataProject)
+    )
 
 }
